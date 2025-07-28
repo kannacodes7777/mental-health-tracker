04eab39e1afac1a1c10c03f084cfab3244f992c2
Hillary Elizabeth <hillary.elizabeth@ui.ac.id>
2025-07-28 10:58:35 +0700

    pindahin allowed host

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%@hlx(wo#9w9_6)&$@b71c@12slk+s13pph94!rdop@zh1u#3%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
